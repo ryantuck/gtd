6c00fe743a0b0b300562a12fd386384a591a596d
Ryan Tuck <ryntck@gmail.com>
2017-10-15 21:55:05 -0400

    add pretty printing

--- create_tables.sql
+++ create_tables.sql
@@ -3,7 +3,8 @@ create table task (
     id              integer primary key autoincrement,
     title           text,
     notes           text            default null,
-    done            boolean         default false
+    done            boolean         default false,
+    project_id      integer         default null
 );
 
 create table project (
@@ -12,10 +13,3 @@ create table project (
     title           text
 );
 
-create table project_task (
-
-    project_id          integer,
-    task_id             integer,
-
-    primary key (project_id, task_id)
-);
