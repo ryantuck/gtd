47aca10b9c19e5843c7f3f24c90b6ab7940c5b72
Ryan Tuck <ryntck@gmail.com>
2019-06-11 13:44:11 -0500

    consolidate ls fns

--- gtd.py
+++ gtd.py
@@ -13,8 +13,8 @@ ORDERS = {
 
 def _read():
     with open(TODO_PATH) as f:
-        lines = f.readlines()
-    return [x.strip() for x in lines]
+        lines = list(enumerate(f.readlines()))
+    return [f'{line.strip()} [{idx+1}]' for idx, line in lines]
 
 
 def parse_attributes(line):
@@ -28,12 +28,6 @@ def parse_contexts(line):
 
 def parse_projects(line):
     return [x for x in line.split() if x.startswith('+')]
-
-
-@click.command('ls')
-def ls():
-    for line in _read():
-        print(line)
 
 
 @click.command('contexts')
@@ -50,20 +44,35 @@ def projects():
         print(f'{v} {k}')
 
 
-@click.command('show-all')
-@click.argument('key')
-def show_all(key):
+@click.command('ls')
+@click.option('--group-by', '-g', default='t', help='Value to group on')
+@click.option('--where', '-w', default=None, help='List of values to filter on')
+def ls(group_by, where):
+
+    where = [] if where is None else where.split(',')
+    included = [w for w in where if not w.startswith('~')]
+    excluded = [w.lstrip('~') for w in where if w.startswith('~')]
+
     all_vals = set(
-        v for line in _read() for k, v in parse_attributes(line).items() if k == key
+        v
+        for line in _read()
+        for k, v in parse_attributes(line).items()
+        if k == group_by
     )
     vals = sorted(all_vals)
-    if key in ORDERS:
-        vals = [v for v in ORDERS[key] if v in all_vals]
+    if group_by in ORDERS:
+        vals = [v for v in ORDERS[group_by] if v in all_vals]
     for val in vals:
-        print(f'{key}:{val}')
-        for line in _read():
-            if parse_attributes(line).get(key) == val:
-                line = ' '.join(x for x in line.split() if not x.startswith(f'{key}:'))
+        print(f'{group_by}:{val}')
+        for line in sorted(_read()):
+            if parse_attributes(line).get(group_by) == val:
+                if included != [] and not any(w in line for w in included):
+                    continue
+                if excluded != [] and any(w in line for w in excluded):
+                    continue
+                line = ' '.join(
+                    x for x in line.split() if not x.startswith(f'{group_by}:')
+                )
                 print(f'    {line}')
 
 
@@ -87,7 +96,6 @@ cli.add_command(ls)
 cli.add_command(contexts)
 cli.add_command(projects)
 cli.add_command(missing_key)
-cli.add_command(show_all)
 
 if __name__ == '__main__':
     cli()
